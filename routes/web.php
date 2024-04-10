59ac6dd92b2a46c6d9ea450719a5918070138b86
Luis Parrado <luisprmat@gmail.com>
2024-04-09 19:04:25 -0500

    Create Student Timetable Page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Student\TimetableController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/student/timetable', [TimetableController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('student.timetable');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
